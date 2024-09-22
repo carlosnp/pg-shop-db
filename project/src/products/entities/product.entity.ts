0b87a8fa961e4ceeb88d3379b81364a9c8da4b52
Carlos N <machinery.e@gmail.com>
2024-09-22 15:14:46 +0000

    Crear tabla de producto

--- project/src/products/entities/product.entity.ts
+++ project/src/products/entities/product.entity.ts
@@ -2,8 +2,31 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'product' })
 export class Product {
+  /** Identificador */
   @PrimaryGeneratedColumn('uuid')
   id: string;
-  @Column('text', { unique: true })
+  /** Titulo */
+  @Column({ type: 'text', unique: true })
   title: string;
+  /** Descripcion. Opcional */
+  @Column({ type: 'text', nullable: true })
+  description: string;
+  /** Slug */
+  @Column({ type: 'text', unique: true })
+  slug: string;
+  /** Precio base */
+  @Column({ type: 'numeric' })
+  price: number;
+  /** Existencias. Opcional */
+  @Column({ type: 'int', default: 0 })
+  stock: number;
+  /** Unidad del producto. Opcional */
+  @Column({ type: 'text', default: 'UNIT' })
+  unit: string;
+  /** Tamaños. Opcional */
+  @Column({ type: 'text', array: true, default: [] })
+  sizes: string[];
+  /** Etiquetas. Opcional */
+  @Column({ type: 'text', array: true, default: [] })
+  tags: string[];
 }
